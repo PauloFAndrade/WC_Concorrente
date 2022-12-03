60d0c6dbe7c71e730e07fd6ceb37c179bfec7f94
Paulo Vitor Fernandes Andrade <69232148+PauloFAndrade@users.noreply.github.com>
2022-12-03 15:45:52 -0300

    maybe the first version

--- src/Manager.java
+++ src/Manager.java
@@ -1,7 +1,14 @@
+import java.util.List;
+import java.util.Random;
+import java.util.ArrayList;
+
 public class Manager {
+	
+	private static int idMan = 1;
+	private static int idWoman = 1;
     
     public static int getWCMaxCapacity(String[] args) {
-        int wcMaxCapacity = -1;
+        int wcMaxCapacity = 5;
         try {
             int index = 0;
             for(String arg : args) {
@@ -10,19 +17,45 @@ public class Manager {
                     wcMaxCapacity = Integer.parseInt(args[index]);
                 }
             }
-            if(wcMaxCapacity == -1) {
-                throw new IllegalArgumentException();
-            }
         } catch (IllegalArgumentException error) {
             if(error instanceof NumberFormatException) {
                 System.out.println("The argument must be a integer");
-                System.exit(-1);
-            } else {
-                System.out.println("You have to pass the maximum capacity as argument using -C");
                 System.exit(-1);
             }
         }
         return wcMaxCapacity;
     }
+    
+    public static void main(String[] args) {
+    	WC wc = new WC(getWCMaxCapacity(args));
+    	List<Person> person = new ArrayList<>();
+    	Random generator = new Random();
+    	
+    	//person.add(new Person(false,wc,id));
+    	    	
+    	for (int i = 0; i < (generator.nextInt(10)+10); i++) {
+            if (generator.nextInt(10)%2 == 0) {
+            	System.out.println("Criou Homem...");
+                person.add(new Person(false, wc, idMan));
+                idMan++;
+            } else {
+            	System.out.println("Criou Mulher...");
+            	person.add(new Person(true, wc, idWoman));
+                idWoman++;
+            }
+        }
+    	
+    	person.stream().map((Person) -> new Thread(Person)).forEach((thread) -> {
+    		thread.start();
+        });
+    	
+    	person.stream().map((Person) -> new Thread(Person)).forEach((thread) -> {
+            try {
+            	thread.join();
+            } catch (InterruptedException e) {
+                System.out.println("Error -> " + e);
+            }
+        });
+	}
     
 }
--- src/WC.java
+++ src/WC.java
@@ -1,6 +1,107 @@
-public class WC extends Thread {
-    // provided as input via command line or prefixed as a constant value
-    private int maxCapacity;
+import java.util.LinkedHashSet;
+import java.util.concurrent.Semaphore;
 
-    private int currentCapacity;    
+public class WC {
+    // provided as input via command line or prefixed as a constant value (5)
+    private int maxCapacity;
+    
+    //
+    private int currentCapacity; 
+    
+    //
+    private boolean currentGender;
+    
+    //
+    private LinkedHashSet<Person> person;
+    
+    //
+    private Semaphore semaphore;
+    
+    //
+    private Semaphore leftMutex;
+    
+    //
+    private Semaphore enterMutex;
+
+	public WC(int maxCapacity) {
+		this.maxCapacity = maxCapacity;
+		this.currentCapacity = maxCapacity;
+		this.currentGender = false;
+		this.person = new LinkedHashSet<>();
+		this.semaphore = new Semaphore(this.maxCapacity, true);
+		this.leftMutex = new Semaphore(1, true);
+		this.enterMutex = new Semaphore(1, true);
+	}
+    
+	public boolean isEmpty() {
+		return person.isEmpty();
+	}
+	
+	public boolean isFull() {
+		return person.size() == maxCapacity;
+	}
+	
+	public boolean currentGender() {
+		return currentGender;
+	}
+	
+	public boolean isInWC(Person person) {
+		return this.person.contains(person);
+	}
+	
+    public void enterPerson(Person person) {
+    	try {
+            this.semaphore.acquire();
+            
+            if(isEmpty()) {
+            	currentGender = person.getGender();
+            }
+            
+            if(!isFull() && !this.person.contains(person) && currentGender == person.getGender()){
+            	enterMutex.acquire();
+            	
+            	if(this.person.add(person)){
+            		boolean gender = person.getGender();
+            		currentCapacity--;
+            		if(gender == false) {
+            			System.out.println("Homem " + person.getID() + " entrou no banheiro! ("+(this.maxCapacity - this.currentCapacity) + " pessoas no banheiro!)");
+            		}else {
+            			System.out.println("Mulher " + person.getID() + " entrou no banheiro! ("+(this.maxCapacity - this.currentCapacity) + " pessoas no banheiro!)");
+            		}
+            		this.enterMutex.release();
+            		//currentCapacity--;
+            		//if((this.maxCapacity - this.currentCapacity) > 1) {
+            		//	System.out.println("Atualmente tem " + (this.maxCapacity - this.currentCapacity) + " pessoas no banheiro!");
+            		//}else {
+            		//	System.out.println("Atualmente tem " + (this.maxCapacity - this.currentCapacity) + " pessoas no banheiro!");
+            		//}
+            	}
+            }
+        } catch (InterruptedException e) {
+            System.out.println("Error -> " + e);
+        }
+    }
+    
+    public void leavePerson(Person person) {
+    	this.semaphore.release();
+    	try {
+			this.leftMutex.acquire();
+			if(this.person.remove(person)) {
+				boolean gender = person.getGender();
+        		if(gender == false) {
+        			System.out.println("Homem " + person.getID() + " saiu do banheiro!");
+        		}else {
+        			System.out.println("Mulher " + person.getID() + " saiu do banheiro!");
+        		}
+        		this.leftMutex.release();
+        		
+        		currentCapacity++;
+        		System.out.println("Atualmente tem " + (this.maxCapacity - this.currentCapacity) + " pessoas no banheiro!");
+			}
+		} catch (InterruptedException e) { 
+			System.out.println("Error -> " + e);
+		}
+    }
+    
+    
 }
